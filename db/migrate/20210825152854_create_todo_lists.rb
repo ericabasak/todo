88bdf1f864315b218d2136d806fae52e9ff54823
ericabasak <ericabasak@gmail.com>
2021-08-25 11:33:49 -0400

    second commit for todo app

---
+++ db/migrate/20210825152854_create_todo_lists.rb
@@ -0,0 +1,11 @@
+class CreateTodoLists < ActiveRecord::Migration[6.0]
+  def change
+    create_table :todo_lists do |t|
+      t.string :title
+      t.string :string
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end

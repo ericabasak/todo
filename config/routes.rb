f6ba93eb208d6caa8efabaeb78c1983f4f055da9
ericabasak <ericabasak@gmail.com>
2021-08-27 23:36:59 -0400

    migration folder updated and routes and db and controller todo items

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   resources :todo_lists do
-    resources :todo_items
+    resources :todo_items do
+      member do
+        patch :completed_at
+      end
+    end
   end
-  
+
   root "todo_lists#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
